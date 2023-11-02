c72340bfc0f9f2d30371ad7ab3232a586d3e6759
Lina <lina.lumburovska@kern.ai>
2023-11-02 17:26:07 +0100

    Small fix

--- src/components/projects/projectId/heuristics/heuristicId/zero-shot/Playground.tsx
+++ src/components/projects/projectId/heuristics/heuristicId/zero-shot/Playground.tsx
@@ -118,7 +118,7 @@ export default function Playground() {
         <Modal modalName={ModalEnum.WHY_SO_LONG}>
             <h1 className="text-lg text-gray-900 mb-2">Why is this taking so long?</h1>
             <div className="text-sm text-gray-500 my-2">
-                Zero shot modules take a lot of time. That's unfortunate but nothing problematic. However, if the test center takes longer than expected usually the reason is that
+                Zero shot modules take a lot of time. That&apos;s unfortunate but nothing problematic. However, if the test center takes longer than expected usually the reason is that
                 the underlying models needs to be prepared. But fear not! This is a task that only needs to be done once.
             </div>
         </Modal>
