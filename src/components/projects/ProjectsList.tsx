b20044f01f4ac3e9c4f5ae1f804aa64bca17e8e8
Lina <lina.lumburovska@kern.ai>
2023-11-14 17:34:16 +0100

    Modal for admin delete project

--- src/components/projects/ProjectsList.tsx
+++ src/components/projects/ProjectsList.tsx
@@ -1,12 +1,12 @@
-import { selectInactiveOrganization, selectIsAdmin, selectIsDemo, selectIsManaged, selectUser } from "@/src/reduxStore/states/general"
-import { removeFromAllProjectsById, selectAllProjects, setAllProjects } from "@/src/reduxStore/states/project";
+import { selectInactiveOrganization, selectIsDemo, selectIsManaged, selectUser } from "@/src/reduxStore/states/general"
+import { selectAllProjects, setAllProjects } from "@/src/reduxStore/states/project";
 import { WebSocketsService } from "@/src/services/base/web-sockets/WebSocketsService";
 import { GET_OVERVIEW_STATS, GET_PROJECT_LIST } from "@/src/services/gql/queries/projects";
 import { Project, ProjectStatistics } from "@/src/types/components/projects/projects-list";
 import { CurrentPage } from "@/src/types/shared/general";
 import { jsonCopy, percentRoundString } from "@/submodules/javascript-functions/general";
 import { useLazyQuery, useMutation } from "@apollo/client";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import YoutubeIntroduction from "./YoutubeIntroduction";
 import ButtonsContainer from "./ButtonsContainer";
@@ -15,15 +15,9 @@ import { GET_CAN_CREATE_LOCAL_ORG } from "@/src/services/gql/queries/organizatio
 import { ADD_USER_TO_ORGANIZATION, CREATE_ORGANIZATION } from "@/src/services/gql/mutations/organizations";
 import style from "@/src/styles/components/projects/projects-list.module.css";
 import { useRouter } from "next/router";
-import Modal from "../shared/modal/Modal";
-import { ModalButton, ModalEnum } from "@/src/types/shared/modal";
-import { DELETE_PROJECT } from "@/src/services/gql/mutations/projects";
-import { closeModal, selectModal } from "@/src/reduxStore/states/modal";
 import { unsubscribeWSOnDestroy } from "@/src/services/base/web-sockets/web-sockets-helper";
 import { postProcessProjectsList } from "@/src/util/components/projects/projects-list-helper";
-
-const ACCEPT_BUTTON = { buttonCaption: "Delete and never show again", useButton: true };
-const ABORT_BUTTON = { buttonCaption: "Delete", useButton: true };
+import AdminDeleteProject from "./AdminDeleteProject";
 
 export default function ProjectsList() {
     const router = useRouter();
@@ -32,10 +26,8 @@ export default function ProjectsList() {
     const organizationInactive = useSelector(selectInactiveOrganization);
     const isManaged = useSelector(selectIsManaged);
     const isDemo = useSelector(selectIsDemo);
-    const isAdmin = useSelector(selectIsAdmin);
     const projects = useSelector(selectAllProjects);
     const user = useSelector(selectUser);
-    const modal = useSelector(selectModal(ModalEnum.ADMIN_DELETE_PROJECT));
 
     const [projectStatisticsById, setProjectStatisticsById] = useState({});
     const [canCreateOrg, setCanCreateOrg] = useState(false);
@@ -46,7 +38,6 @@ export default function ProjectsList() {
     const [refetchCanCreateOrg] = useLazyQuery(GET_CAN_CREATE_LOCAL_ORG, { fetchPolicy: "no-cache" });
     const [createOrgMut] = useMutation(CREATE_ORGANIZATION);
     const [addUserToOrgMut] = useMutation(ADD_USER_TO_ORGANIZATION);
-    const [deleteProjectByIdMut] = useMutation(DELETE_PROJECT, { fetchPolicy: "no-cache" });
 
     useEffect(unsubscribeWSOnDestroy(router, [CurrentPage.PROJECTS]), []);
 
@@ -58,28 +49,6 @@ export default function ProjectsList() {
             createDefaultOrg();
         }
     }, [organizationInactive]);
-
-    const adminDeleteProject = useCallback(() => {
-        if (!isAdmin) return;
-        const projectId = modal.projectId;
-        deleteProjectByIdMut({ variables: { projectId: projectId } }).then(() => {
-            dispatch(closeModal(ModalEnum.ADMIN_DELETE_PROJECT));
-            dispatch(removeFromAllProjectsById(projectId));
-        })
-    }, [isAdmin, modal]);
-
-    const adminStoreInstantAndDelete = useCallback(() => {
-        localStorage.setItem("adminInstantDelete", "X");
-        adminDeleteProject();
-    }, [adminDeleteProject]);
-
-    const [acceptButton, setAcceptButton] = useState<ModalButton>(ACCEPT_BUTTON);
-    const [abortButton, setAbortButton] = useState<ModalButton>(ABORT_BUTTON);
-
-    useEffect(() => {
-        setAcceptButton({ ...ACCEPT_BUTTON, emitFunction: adminStoreInstantAndDelete });
-        setAbortButton({ ...ABORT_BUTTON, emitFunction: adminDeleteProject });
-    }, [modal]);
 
     function initData() {
         refetchProjectsAndPostProcess();
@@ -252,14 +221,7 @@ export default function ProjectsList() {
                                         <ProjectCard project={project} projectStatisticsById={projectStatisticsById} key={index}></ProjectCard>
                                     ))}
                                 </div>
-                                <Modal modalName={ModalEnum.ADMIN_DELETE_PROJECT} acceptButton={acceptButton} abortButton={abortButton}>
-                                    <div className="flex flex-row items-center justify-center">
-                                        <span className="text-lg leading-6 text-gray-900 font-medium">
-                                            Admin Function - Quick delete
-                                        </span>
-                                    </div>
-                                    Are you sure?<div>This will delete the project and all its data.</div>
-                                </Modal>
+                                <AdminDeleteProject />
                             </div>
                         </div>)}
                     </>) : (<></>)}
