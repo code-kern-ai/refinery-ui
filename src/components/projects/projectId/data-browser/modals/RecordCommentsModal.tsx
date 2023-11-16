0379780f7b6430f0f397b21f557bcbd7f4e5394f
Lina <lina.lumburovska@kern.ai>
2023-11-16 17:50:28 +0100

    Key on map added

--- src/components/projects/projectId/data-browser/modals/RecordCommentsModal.tsx
+++ src/components/projects/projectId/data-browser/modals/RecordCommentsModal.tsx
@@ -9,7 +9,7 @@ export default function RecordCommentsModal() {
     return (<Modal modalName={ModalEnum.RECORD_COMMENTS}>
         <h1 className="text-lg text-gray-900 mb-2 text-center">Record comments</h1>
         {modalComments && modalComments.commentsData && modalComments.commentsData.map((comment, index) => (
-            <div className="break-words">
+            <div className="break-words" key={comment.comment}>
                 <div className="flex flex-grow justify-between gap-8">
                     <p style={{ minWidth: '100px' }} className="font-bold">Comment {index + 1}</p>
                     <p className="text-gray-500">{comment.comment}</p>
