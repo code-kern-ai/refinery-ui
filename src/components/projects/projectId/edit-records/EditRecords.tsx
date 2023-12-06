73e085ed60ac044820c9b801002067cc33c556de
Lina <lina.lumburovska@kern.ai>
2023-12-06 19:17:34 +0100

    Web sockets for edit records and other smaller changes

--- src/components/projects/projectId/edit-records/EditRecords.tsx
+++ src/components/projects/projectId/edit-records/EditRecords.tsx
@@ -2,45 +2,91 @@ import { selectProjectId } from "@/src/reduxStore/states/project"
 import { useSelector } from "react-redux"
 import NavBarTopEditRecords from "./NavBarTopEditRecords";
 import { selectSessionData } from "@/src/reduxStore/states/tmp";
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useCallback, useEffect, useState } from "react";
 import { buildAccessKey, createDefaultEditRecordComponentData } from "@/src/util/components/projects/projectId/edit-records-helper";
 import style from '@/src/styles/components/projects/projectId/edit-records.module.css';
 import { scrollElementIntoView } from "@/submodules/javascript-functions/scrollHelper";
-import { DataTypeEnum } from "@/src/types/shared/general";
+import { CurrentPage, DataTypeEnum } from "@/src/types/shared/general";
 import { IconAlertCircle, IconAlertTriangle, IconAlertTriangleFilled, IconBallpen, IconBallpenOff } from "@tabler/icons-react";
 import { Tooltip } from "@nextui-org/react";
 import { TOOLTIPS_DICT } from "@/src/util/tooltip-constants";
 import EditField from "./EditField";
+import { WebSocketsService } from "@/src/services/base/web-sockets/WebSocketsService";
+import { useRouter } from "next/router";
+
 
 export default function EditRecords() {
+    const router = useRouter();
+
     const projectId = useSelector(selectProjectId);
     const sessionData = useSelector(selectSessionData);
 
     const [erdData, setErdData] = useState(createDefaultEditRecordComponentData());
+    const [alertLastVisible, setAlertLastVisible] = useState<number>(null);
 
     useEffect(() => {
-        if (!sessionData) return;
+        if (!projectId) return;
+        if (!sessionData) {
+            console.warn("No session data available. Redirecting to data browser.");
+            router.push(`/projects/${projectId}/data-browser`);
+            return;
+        }
         const erdDataCopy = { ...erdData };
         erdDataCopy.data = sessionData;
         erdDataCopy.displayRecords = erdDataCopy.data.records;
         erdDataCopy.editRecordId = erdDataCopy.data.selectedRecordId;
         erdDataCopy.navBar.positionString = erdDataCopy.data.records.length + " records in";
         setErdData(erdDataCopy);
-        scrollElementIntoView("flash-it", 50);
-    }, [sessionData]);
+        WebSocketsService.subscribeToNotification(CurrentPage.EDIT_RECORDS, {
+            projectId: projectId,
+            whitelist: ['calculate_attribute'],
+            func: handleWebsocketNotification
+        });
+    }, [sessionData, projectId]);
+
+    useEffect(() => {
+        function handleBeforeUnload(event) {
+            const hasUnsavedChanges = Object.keys(erdData.cachedRecordChanges).length > 0;
+            if (hasUnsavedChanges) {
+                event.returnValue = 'You have unsaved changes. Are you sure you want to leave?';
+            }
+        }
+        window.addEventListener('beforeunload', handleBeforeUnload);
+        return () => {
+            window.removeEventListener('beforeunload', handleBeforeUnload);
+        };
+    }, [erdData.cachedRecordChanges]);
+
+    const handleWebsocketNotification = useCallback((msgParts: string[]) => {
+        if (msgParts[1] == 'calculate_attribute' && msgParts[2] == 'finished') {
+            window.location.reload();
+            alertUser(msgParts[1]);
+        }
+    }, []);
+
+    const alertUser = useCallback((msg: string) => {
+        if (alertLastVisible && Date.now() - alertLastVisible < 1000) return;
+        alert("Settings were changed (msgId: " + msg + ")\nPage will be reloaded.");
+        setAlertLastVisible(Date.now());
+    }, [alertLastVisible]);
+
+    useEffect(() => {
+        if (!projectId) return;
+        WebSocketsService.updateFunctionPointer(projectId, CurrentPage.EDIT_RECORDS, handleWebsocketNotification)
+    }, [handleWebsocketNotification, projectId]);
 
     return (<>{projectId && <div className="h-screen bg-white flex flex-col">
         <NavBarTopEditRecords erdData={erdData} setErdData={(erdData) => setErdData(erdData)} />
         <div className={`grid items-start pt-2 px-2 pb-8 gap-2 flex-grow overflow-y-auto auto-rows-max ${erdData.columnClass}`} style={{ maxHeight: 'calc(100vh - 95px)' }}>
             {erdData.data && <>
                 {erdData.displayRecords.map((record) =>
-                    <div key={record.id} className={`relative space-x-3 items-center bg-white overflow-hidden shadow rounded-lg border w-full scroll-mt-8 ${erdData.displayRecords?.length > 2 ? style.item : ''}`}
+                    <div key={record.id} className={`relative space-x-3 items-center bg-white overflow-hidden shadow rounded-lg border w-full scroll-mt-8 ${erdData.displayRecords?.length > 2 ? style.item : ''} `}
                         onDoubleClick={() => {
                             if (erdData.editRecordId === record.id) return;
                             const erdDataCopy = { ...erdData };
                             erdDataCopy.editRecordId = record.id;
                             setErdData(erdDataCopy);
-                        }} id={record.id == erdData.data.selectedRecordId ? 'flash-id' : null}>
+                        }} id={record.id == erdData.data.selectedRecordId ? 'flash-it' : null}>
                         <div className="px-4 py-5 sm:p-6">
                             {erdData.data.attributes.map((attribute) => (<Fragment key={attribute.id}>
                                 <div>
