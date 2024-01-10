68ae1ea03b52b6c45bbcff9deaca2eefc49068aa
Lina <lina.lumburovska@kern.ai>
2024-01-10 18:13:20 +0100

    Adding multiple labels per token fix

--- src/components/projects/projectId/labeling/sessionId/sub-components/ExtractionDisplay.tsx
+++ src/components/projects/projectId/labeling/sessionId/sub-components/ExtractionDisplay.tsx
@@ -3,29 +3,19 @@ import { LineBreaksType } from "@/src/types/components/projects/projectId/data-b
 import { ExtractionDisplayProps } from "@/src/types/components/projects/projectId/labeling/labeling";
 import { LabelingPageParts } from "@/src/types/components/projects/projectId/labeling/labeling-main-component";
 import { LabelSource } from "@/submodules/javascript-functions/enums/enums";
-import { jsonCopy } from "@/submodules/javascript-functions/general";
 import { Tooltip } from "@nextui-org/react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import style from '@/src/styles/components/projects/projectId/labeling.module.css';
+import { useState } from "react";
 
 export default function ExtractionDisplay(props: ExtractionDisplayProps) {
-    const dispatch = useDispatch();
-
     const settings = useSelector(selectSettings);
     const hoverGroupsDict = useSelector(selectHoverGroupDict);
 
+    const [hoverBoxDict, setHoverGroupDict] = useState({});
+
     function deleteRecordLabelAssociation(rlaId: string) {
         props.deleteRla(rlaId);
-    }
-
-    function onMouseEvent(update: boolean, labelId?: string) {
-        const hoverGroupsDictCopy = jsonCopy(hoverGroupsDict);
-        hoverGroupsDictCopy[labelId][LabelingPageParts.TASK_HEADER] = update;
-        hoverGroupsDictCopy[labelId][LabelingPageParts.OVERVIEW_TABLE] = update;
-        hoverGroupsDictCopy[labelId][LabelingPageParts.TABLE_MODAL] = update;
-        hoverGroupsDictCopy[labelId][LabelingPageParts.MANUAL] = update;
-        hoverGroupsDictCopy[labelId][LabelingPageParts.WEAK_SUPERVISION] = update;
-        dispatch(setHoverGroupDict(hoverGroupsDictCopy));
     }
 
     return (<>
@@ -39,16 +29,23 @@ export default function ExtractionDisplay(props: ExtractionDisplayProps) {
                     </Tooltip>) : (<>
                         <TokenValue token={token} attributeId={props.attributeId} setSelected={(e) => props.setSelected(token.idx, token.idx, e)} /></>)}
                     {props.tokenLookup[props.attributeId][token.idx] && <>
-                        {props.tokenLookup[props.attributeId][token.idx].rlaArray.map((rlaItem) => (<div key={rlaItem.orderPos} className={`absolute left-0 right-0 top-0 flex items-end z-n-2`} style={{ bottom: rlaItem.bottomPos }}>
-                            <div className={`h-px flex items-end w-full relative ${props.labelLookup[rlaItem.labelId].color.backgroundColor} ${props.labelLookup[rlaItem.labelId].color.textColor} ${props.labelLookup[rlaItem.labelId].color.borderColor} ${hoverGroupsDict[rlaItem.labelId][LabelingPageParts.MANUAL] || hoverGroupsDict[rlaItem.labelId][LabelingPageParts.WEAK_SUPERVISION] ? 'heightHover' : ''}`}
-                                onMouseEnter={(e: any) => {
-                                    e.target.classList.add('heightHover');
-                                    onMouseEvent(true, rlaItem.labelId);
-                                }} onMouseLeave={(e: any) => {
-                                    e.target.classList.remove('heightHover');
-                                    onMouseEvent(false, rlaItem.labelId);
-                                }}
+                        {props.tokenLookup[props.attributeId][token.idx].rlaArray.map((rlaItem) => (<div key={rlaItem.orderPos} className={`absolute left-0 right-0 top-0 flex items-end`} style={{ bottom: rlaItem.bottomPos, zIndex: -2 }}
+                            onMouseEnter={(e: any) => {
+                                const hoverBoxDictCopy = { ...hoverBoxDict };
+                                hoverBoxDictCopy[rlaItem.rla.id] = true;
+                                setHoverGroupDict(hoverBoxDictCopy);
+                                e.target.style.zIndex = -1;
+                            }} onMouseLeave={(e: any) => {
+                                const hoverBoxDictCopy = { ...hoverBoxDict };
+                                for (const key in hoverBoxDictCopy) {
+                                    hoverBoxDictCopy[key] = false;
+                                }
+                                setHoverGroupDict(hoverBoxDictCopy);
+                                e.target.style.zIndex = -2;
+                            }}>
+                            <div className={`h-px flex items-end w-full relative ${props.labelLookup[rlaItem.labelId].color.backgroundColor} ${props.labelLookup[rlaItem.labelId].color.textColor} ${props.labelLookup[rlaItem.labelId].color.borderColor} ${hoverGroupsDict[rlaItem.labelId][LabelingPageParts.MANUAL] || hoverGroupsDict[rlaItem.labelId][LabelingPageParts.WEAK_SUPERVISION] || hoverBoxDict[rlaItem.rla.id] ? 'heightHover' : ''}`}
                                 style={{
+                                    zIndex: -2,
                                     borderBottomWidth: '1px',
                                     borderTopWidth: '1px',
                                     left: '-2px',
@@ -59,14 +56,14 @@ export default function ExtractionDisplay(props: ExtractionDisplayProps) {
                                     borderRightWidth: rlaItem.isLast ? '1px' : null,
                                     borderTopRightRadius: rlaItem.isLast ? '0.375rem' : null,
                                     borderBottomRightRadius: rlaItem.isLast ? '0.375rem' : null,
-                                }}>
-                                {rlaItem.isFirst && rlaItem.canBeDeleted && <div className="pl-1 cursor-pointer absolute" style={{ bottom: '-11px', left: '-21px' }} onClick={() => deleteRecordLabelAssociation(rlaItem.rla.id)}>
+                                }} onMouseEnter={(e: any) => e.target.zIndex = -1}>
+                                {rlaItem.isFirst && rlaItem.canBeDeleted && <div onMouseEnter={(e: any) => e.target.zIndex = -2} className="pl-1 cursor-pointer absolute" style={{ bottom: '-11px', left: '-21px' }} onClick={() => deleteRecordLabelAssociation(rlaItem.rla.id)}>
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 inline-block stroke-current" style={{ transform: 'rotate(180deg)' }} viewBox="0 0 20 20" fill="currentColor">
                                         <path fillRule="evenodd" d="M6.707 4.879A3 3 0 018.828 4H15a3 3 0 013 3v6a3 3 0 01-3 3H8.828a3 3 0 01-2.12-.879l-4.415-4.414a1 1 0 010-1.414l4.414-4.414zm4 2.414a1 1 0 00-1.414 1.414L10.586 10l-1.293 1.293a1 1 0 101.414 1.414L12 11.414l1.293 1.293a1 1 0 001.414-1.414L13.414 10l1.293-1.293a1 1 0 00-1.414-1.414L12 8.586l-1.293-1.293z"
                                             clipRule="evenodd" />
                                     </svg>
                                 </div>}
-                                <div className={`label-overlay-base ${hoverGroupsDict[rlaItem.labelId][LabelingPageParts.MANUAL] && rlaItem.rla.sourceType == LabelingPageParts.MANUAL && style.labelOverlayManual} ${hoverGroupsDict[rlaItem.labelId][LabelingPageParts.WEAK_SUPERVISION] && rlaItem.rla.sourceType == LabelingPageParts.WEAK_SUPERVISION && style.labelOverlayWeakSupervision}`}></div>
+                                <div className={`label-overlay-base ${hoverGroupsDict[rlaItem.labelId][LabelingPageParts.MANUAL] && rlaItem.rla.sourceType == LabelingPageParts.MANUAL && style.labelOverlayManual} ${hoverGroupsDict[rlaItem.labelId][LabelingPageParts.WEAK_SUPERVISION] && rlaItem.rla.sourceType == LabelingPageParts.WEAK_SUPERVISION && style.labelOverlayWeakSupervision} ${hoverBoxDict[rlaItem.rla.id] ? style.labelOverlayManual : ''}`}></div>
                             </div>
                         </div>
                         ))}
@@ -84,9 +81,9 @@ export default function ExtractionDisplay(props: ExtractionDisplayProps) {
 function TokenValue(props: any) {
     return (<>
         {props.token && props.token.value != '\n' && <label onClick={(e) => props.setSelected(e)}
-            className={`rounded-lg hover:bg-gray-200 text-sm text-gray-500 leading-5 font-normal ${!props.token.nextCloser ? 'pr-1' : ''}`}
+            className={`rounded-lg hover:bg-gray-200 text-sm text-gray-500 leading-5 relative font-normal ${!props.token.nextCloser ? 'pr-1' : ''}`}
             data-tokenidx={props.token.idx} data-attributeid={props.attributeId}
-            style={{ backgroundColor: props.token.selected ? '#3399FF' : null, borderRadius: props.token.selected ? '0' : null, color: props.token.selected ? 'white' : null }}>
+            style={{ backgroundColor: props.token.selected ? '#3399FF' : null, borderRadius: props.token.selected ? '0' : null, color: props.token.selected ? 'white' : null, zIndex: '100' }}>
             {props.token.value}
         </label>}
     </>)
