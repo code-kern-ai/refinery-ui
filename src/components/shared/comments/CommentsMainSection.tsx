ceda590ccb0549645058a76c114d027de7b13822
Lina <lina.lumburovska@kern.ai>
2024-01-24 14:34:17 +0100

    Comments animation

--- src/components/shared/comments/CommentsMainSection.tsx
+++ src/components/shared/comments/CommentsMainSection.tsx
@@ -103,52 +103,77 @@ export default function CommentsMainSection(props: CommentMainSectionProps) {
         });
     }, [commentTextsArray]);
 
+    const [transformValue, setTransformValue] = useState(null);
+
+    useEffect(() => {
+        if (props.open && positionComment === CommentPosition.RIGHT) {
+            setTransformValue({
+                transform: 'translateX(0%)',
+                transition: 'transform 500ms ease',
+            });
+        } else if (props.open && positionComment === CommentPosition.LEFT) {
+            setTransformValue({
+                transform: 'translateX(0%)',
+                transition: 'transform 500ms ease',
+
+            });
+        }
+        else if (!props.open && positionComment === CommentPosition.RIGHT) {
+            setTransformValue({
+                transform: 'translateX(100%)',
+                transition: 'transform 500ms ease',
+            });
+        } else {
+            setTransformValue({
+                transform: 'translateX(-100%)',
+                transition: 'transform 500ms ease',
+            });
+        }
+    }, [props.open, positionComment]);
+
     return (
         <div className="relative z-30" role="dialog" aria-modal="true">
             <div className="inset-0 bg-gray-500 bg-opacity-0 opacity-0"></div>
-
-            <div className={`absolute inset-0 overflow-hidden ${props.open ? 'block' : 'hidden'}`}>
-                <div className="absolute inset-0 overflow-hidden">
-                    <div className={`pointer-events-auto fixed inset-y-0 flex max-w-full border-x w-96 top-16 ${positionComment == CommentPosition.RIGHT ? 'right-0' : 'left-20'}`}>
-                        <div className="flex h-full flex-col overflow-y-auto bg-white shadow-xl">
-                            <div className="flex flex-row items-center px-4 pt-4">
-                                {positionComment == CommentPosition.RIGHT ? <Tooltip content={TOOLTIPS_DICT.GENERAL.MOVE_COMMENT_LEFT} color="invert">
-                                    <div className='cursor-pointer' onClick={flipCommentPosition}>
-                                        <IconArrowLeft size={24}
-                                            strokeWidth={2}
-                                            className='text-gray-700 font-bold cursor-pointer' />
-                                    </div>
-                                </Tooltip> : <Tooltip content={TOOLTIPS_DICT.GENERAL.MOVE_COMMENT_RIGHT} color="invert">
-                                    <div className='cursor-pointer' onClick={flipCommentPosition}>
-                                        <IconArrowRight size={24}
-                                            strokeWidth={2}
-                                            className='text-gray-700 font-bold cursor-pointer' />
-                                    </div>
-                                </Tooltip>}
-                                <div className="text-base flex m-auto font-bold leading-6 text-gray-900 cursor-pointer" onClick={() => openAllComments(!allOpen)}>
-                                    <Tooltip content={allOpen ? TOOLTIPS_DICT.GENERAL.CLOSE_ALL : TOOLTIPS_DICT.GENERAL.OPEN_ALL} contentColor="invert" hideArrow={true} placement='bottom'
-                                        css={{ color: '#6B7280', border: '1px solid #6B7280', backgroundColor: '#F3F4F6', textAlign: 'center' }}>
-                                        Comments
-                                    </Tooltip>
+            <div className={`absolute inset-0 overflow-hidden`}>
+                {props.open && <div className={`pointer-events-auto fixed inset-y-0 flex max-w-full border-x w-96 top-16 ${positionComment == CommentPosition.RIGHT ? 'right-0' : 'left-20'}`} style={transformValue}>
+                    <div className="flex h-full flex-col overflow-y-auto bg-white shadow-xl">
+                        <div className="flex flex-row items-center px-4 pt-4">
+                            {positionComment == CommentPosition.RIGHT ? <Tooltip content={TOOLTIPS_DICT.GENERAL.MOVE_COMMENT_LEFT} color="invert">
+                                <div className='cursor-pointer' onClick={flipCommentPosition}>
+                                    <IconArrowLeft size={24}
+                                        strokeWidth={2}
+                                        className='text-gray-700 font-bold cursor-pointer' />
                                 </div>
-                                <button onClick={props.toggleOpen} type="button" className="rounded-md text-gray-400">
-                                    <span className="sr-only">Close panel</span>
-                                    <IconX size={24} strokeWidth={2} className='text-gray-400 cursor-pointer' />
-                                </button>
+                            </Tooltip> : <Tooltip content={TOOLTIPS_DICT.GENERAL.MOVE_COMMENT_RIGHT} color="invert">
+                                <div className='cursor-pointer' onClick={flipCommentPosition}>
+                                    <IconArrowRight size={24}
+                                        strokeWidth={2}
+                                        className='text-gray-700 font-bold cursor-pointer' />
+                                </div>
+                            </Tooltip>}
+                            <div className="text-base flex m-auto font-bold leading-6 text-gray-900 cursor-pointer" onClick={() => openAllComments(!allOpen)}>
+                                <Tooltip content={allOpen ? TOOLTIPS_DICT.GENERAL.CLOSE_ALL : TOOLTIPS_DICT.GENERAL.OPEN_ALL} contentColor="invert" hideArrow={true} placement='bottom'
+                                    css={{ color: '#6B7280', border: '1px solid #6B7280', backgroundColor: '#F3F4F6', textAlign: 'center' }}>
+                                    Comments
+                                </Tooltip>
                             </div>
-                            <DisplayComments position={positionComment} openComments={openCommentsArray} editComments={editCommentsArray} commentTexts={commentTextsArray}
-                                handleCommentClick={(index: number) => handleCommentClick(index)}
-                                handleEditClick={(index: number) => handleEditComment(index)}
-                                editComment={(event: Event, commentId: string, toChangeKey: string, toChangeValue: any, index: number) => updateComment(event, commentId, toChangeKey, toChangeValue, index)}
-                                handleCommentTextChange={(value: string, index: number) => handleCommentTextChange(value, index)}
-                                deleteComment={(id: string) => deleteComment(id)} />
-                            <hr></hr>
-                            <CommentCreation
-                                saveComment={(type: string, commentId: string, comment: string, isPrivate: boolean) => saveComment(type, commentId, comment, isPrivate)}
-                                closeCommentCreation={props.toggleOpen} isOpen={props.open} />
+                            <button onClick={props.toggleOpen} type="button" className="rounded-md text-gray-400">
+                                <span className="sr-only">Close panel</span>
+                                <IconX size={24} strokeWidth={2} className='text-gray-400 cursor-pointer' />
+                            </button>
                         </div>
+                        <DisplayComments position={positionComment} openComments={openCommentsArray} editComments={editCommentsArray} commentTexts={commentTextsArray}
+                            handleCommentClick={(index: number) => handleCommentClick(index)}
+                            handleEditClick={(index: number) => handleEditComment(index)}
+                            editComment={(event: Event, commentId: string, toChangeKey: string, toChangeValue: any, index: number) => updateComment(event, commentId, toChangeKey, toChangeValue, index)}
+                            handleCommentTextChange={(value: string, index: number) => handleCommentTextChange(value, index)}
+                            deleteComment={(id: string) => deleteComment(id)} />
+                        <hr></hr>
+                        <CommentCreation
+                            saveComment={(type: string, commentId: string, comment: string, isPrivate: boolean) => saveComment(type, commentId, comment, isPrivate)}
+                            closeCommentCreation={props.toggleOpen} isOpen={props.open} />
                     </div>
-                </div>
+                </div>}
             </div>
         </div>
     )
