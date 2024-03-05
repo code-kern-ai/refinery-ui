fe668c6121a7c97087b825d78d8e778a060108b6
Lina <lina.lumburovska@kern.ai>
2024-03-05 17:09:45 +0100

    Boolean values on labeling page fix

--- src/components/projects/projectId/labeling/sessionId/sub-components/LabelingSuiteLabeling.tsx
+++ src/components/projects/projectId/labeling/sessionId/sub-components/LabelingSuiteLabeling.tsx
@@ -577,7 +577,7 @@ export default function LabelingSuiteLabeling() {
                                     setSelected={(start, end, e) => setSelected(attribute.id, start, end, e)} />) : (<>
                                         {(recordRequests.record.data[lVars.taskLookup[attribute.id].attribute.name] != null && recordRequests.record.data[lVars.taskLookup[attribute.id].attribute.name] !== '') ?
                                             (<p className={`break-words text-sm leading-5 font-normal text-gray-500 ${settings.main.lineBreaks != LineBreaksType.NORMAL ? (settings.main.lineBreaks == LineBreaksType.IS_PRE_WRAP ? 'whitespace-pre-wrap' : 'whitespace-pre-line') : ''}`}>
-                                                {recordRequests.record.data[lVars.taskLookup[attribute.id].attribute.name]}
+                                                {`${recordRequests.record.data[lVars.taskLookup[attribute.id].attribute.name]}`}
                                             </p>) : (<>
                                                 <IconAlertCircle className="text-yellow-700 inline-block h-5 w-5" />
                                                 <span className="text-gray-500 text-sm font-normal italic">Not present in the
