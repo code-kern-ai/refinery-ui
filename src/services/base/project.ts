8894d45ec4f4dd50d93b304a14acd9a6000e80be
anmarhindi <anmar.hindi@kern.ai>
2024-03-20 10:58:45 +0100

    change to get project by project id url

--- src/services/base/project.ts
+++ src/services/base/project.ts
@@ -4,7 +4,7 @@ import { BACKEND_BASE_URI } from "../../../src/services/base/_settings";
 export const projectEndpoint = `${BACKEND_BASE_URI}/api/v1/project`;
 
 export function getProjectByProjectId(projectId: string, onResult: (result: any) => void) {
-    const finalUrl = `${projectEndpoint}/project-by-project-id/${projectId}`;
+    const finalUrl = `${projectEndpoint}/${projectId}/project-by-project-id`;
     jsonFetchWrapper(finalUrl, FetchType.GET, onResult);
 }
 
