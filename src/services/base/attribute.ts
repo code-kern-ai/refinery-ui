e03947bd1bd95867a303634a1efb41a0e5f8e4fe
LennartSchmidtKern <lennart.schmidt@kern.ai>
2024-03-20 17:55:37 +0100

    snake case query param fix

--- src/services/base/attribute.ts
+++ src/services/base/attribute.ts
@@ -5,7 +5,7 @@ export const attributeEndpoint = `${BACKEND_BASE_URI}/api/v1/attribute`;
 
 export function getAttributes(projectId: string, stateFilter: string[], onResult: (result: any) => void) {
     const stateFilterQuery = new URLSearchParams()
-    stateFilter.forEach(filter => stateFilterQuery.append("stateFilter", filter));
+    stateFilter.forEach(filter => stateFilterQuery.append("state_filter", filter));
     const finalUrl = `${attributeEndpoint}/all-attributes/${projectId}?${stateFilterQuery}`;
     jsonFetchWrapper(finalUrl, FetchType.GET, onResult);
 }
