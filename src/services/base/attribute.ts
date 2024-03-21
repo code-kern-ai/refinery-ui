b5f0902b38b55bfdfcb3d309c8920e0c7a275386
LennartSchmidtKern <lennart.schmidt@kern.ai>
2024-03-21 10:39:05 +0100

    attributesByProjectId fix

--- src/services/base/attribute.ts
+++ src/services/base/attribute.ts
@@ -6,6 +6,6 @@ export const attributeEndpoint = `${BACKEND_BASE_URI}/api/v1/attribute`;
 export function getAttributes(projectId: string, stateFilter: string[], onResult: (result: any) => void) {
     const stateFilterQuery = new URLSearchParams()
     stateFilter.forEach(filter => stateFilterQuery.append("state_filter", filter));
-    const finalUrl = `${attributeEndpoint}/all-attributes/${projectId}?${stateFilterQuery}`;
+    const finalUrl = `${attributeEndpoint}/${projectId}/all-attributes?${stateFilterQuery}`;
     jsonFetchWrapper(finalUrl, FetchType.GET, onResult);
 }
