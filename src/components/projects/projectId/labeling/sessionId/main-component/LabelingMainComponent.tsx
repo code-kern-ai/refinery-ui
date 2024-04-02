246389153c256dd5aa926fe22f147c0b44e05fcb
anmarhindi <anmar.hindi@kern.ai>
2024-04-02 11:30:44 +0200

    adds requestHuddleData

--- src/components/projects/projectId/labeling/sessionId/main-component/LabelingMainComponent.tsx
+++ src/components/projects/projectId/labeling/sessionId/main-component/LabelingMainComponent.tsx
@@ -1,7 +1,7 @@
 import { selectAllUsers, selectUser, setBricksIntegrator, setComments } from "@/src/reduxStore/states/general";
 import { setAvailableLinks, updateRecordRequests, setSelectedLink, selectRecordRequestsRla, updateUsers, setSettings, selectSettings, setUserDisplayId, selectRecordRequestsRecord, initOnLabelPageDestruction, selectUserDisplayId, selectDisplayUserRole, setDisplayUserRole } from "@/src/reduxStore/states/pages/labeling";
 import { selectProjectId } from "@/src/reduxStore/states/project"
-import { GET_RECORD_LABEL_ASSOCIATIONS, GET_TOKENIZED_RECORD, LINK_LOCKED, REQUEST_HUDDLE_DATA } from "@/src/services/gql/queries/labeling";
+import { GET_RECORD_LABEL_ASSOCIATIONS, GET_TOKENIZED_RECORD, LINK_LOCKED } from "@/src/services/gql/queries/labeling";
 import { LabelingLinkType } from "@/src/types/components/projects/projectId/labeling/labeling-main-component";
 import { UserRole } from "@/src/types/shared/sidebar";
 import { LabelingSuiteManager } from "@/src/util/classes/labeling/manager";
@@ -31,7 +31,7 @@ import { useWebsocket } from "@/src/services/base/web-sockets/useWebsocket";
 import { getAllComments } from "@/src/services/base/comment";
 import { getAttributes } from "@/src/services/base/attribute";
 import { getLabelingTasksByProjectId } from "@/src/services/base/project";
-import { getAvailableLinks } from "@/src/services/base/labeling";
+import { getAvailableLinks, getHuddleData } from "@/src/services/base/labeling";
 
 const SETTINGS_KEY = 'labelingSettings';
 
@@ -53,7 +53,6 @@ export default function LabelingMainComponent() {
     const [absoluteWarning, setAbsoluteWarning] = useState(null);
     const [lockedLink, setLockedLink] = useState(false);
 
-    const [refetchHuddleData] = useLazyQuery(REQUEST_HUDDLE_DATA, { fetchPolicy: 'no-cache' });
     const [refetchTokenizedRecord] = useLazyQuery(GET_TOKENIZED_RECORD, { fetchPolicy: 'no-cache' });
     const [refetchRecordByRecordId] = useLazyQuery(GET_RECORD_BY_RECORD_ID, { fetchPolicy: 'no-cache' });
     const [refetchRla] = useLazyQuery(GET_RECORD_LABEL_ASSOCIATIONS, { fetchPolicy: 'network-only' });
@@ -236,7 +235,7 @@ export default function LabelingMainComponent() {
     }
 
     function requestHuddleData(huddleId: string) {
-        refetchHuddleData({ variables: { projectId: projectId, huddleId: huddleId, huddleType: SessionManager.labelingLinkData.linkType } }).then((result) => {
+        getHuddleData(projectId, { huddleId: huddleId, huddleType: SessionManager.labelingLinkData.linkType }, (result) => {
             const huddleData = result['data']['requestHuddleData'];
             if (huddleId == DUMMY_HUDDLE_ID) {
                 SessionManager.labelingLinkData.huddleId = huddleData.huddleId;
--- src/services/base/labeling.ts
+++ src/services/base/labeling.ts
@@ -1,9 +1,17 @@
 import { FetchType, jsonFetchWrapper } from "@/submodules/javascript-functions/basic-fetch";
 import { BACKEND_BASE_URI } from "./_settings";
+import { LabelingLinkType } from "@/src/types/components/projects/projectId/labeling/labeling-main-component";
 
 export const labelingEndpoint = `${BACKEND_BASE_URI}/api/v1/labeling`;
 
 export function getAvailableLinks(projectId: string, assumedRole: string, assumedHeuristicId: string, onResult: (result: any) => void) {
     const finalUrl = `${labelingEndpoint}/${projectId}/available-links`;
     jsonFetchWrapper(finalUrl, FetchType.POST, onResult, JSON.stringify({ assumedRole, assumedHeuristicId }));
+}
+
+export function getHuddleData(projectId: string, options: {
+    huddleId: string, huddleType: LabelingLinkType
+}, onResult: (result: any) => void) {
+    const finalUrl = `${labelingEndpoint}/${projectId}/huddle-data`;
+    jsonFetchWrapper(finalUrl, FetchType.POST, onResult, JSON.stringify(options));
 }
