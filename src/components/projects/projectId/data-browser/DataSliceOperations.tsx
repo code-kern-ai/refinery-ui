25693a8ff16ea4e8fc750f5ad0ee730a790c5001
anmarhindi <anmar.hindi@kern.ai>
2024-04-03 16:54:45 +0200

    cleanup

--- src/components/projects/projectId/data-browser/DataSliceOperations.tsx
+++ src/components/projects/projectId/data-browser/DataSliceOperations.tsx
@@ -1,14 +1,12 @@
 import { selectUser } from "@/src/reduxStore/states/general";
 import { openModal, selectModal } from "@/src/reduxStore/states/modal";
-import { selectActiveSearchParams, selectActiveSlice, selectAdditionalData, selectConfiguration, selectDataSlicesAll, setActiveDataSlice, updateAdditionalDataState } from "@/src/reduxStore/states/pages/data-browser";
+import { selectActiveSearchParams, selectActiveSlice, selectAdditionalData, selectConfiguration, setActiveDataSlice, updateAdditionalDataState } from "@/src/reduxStore/states/pages/data-browser";
 import { selectAttributes, selectEmbeddings, selectLabelingTasksAll } from "@/src/reduxStore/states/pages/settings";
 import { selectProjectId } from "@/src/reduxStore/states/project";
-import { UPDATE_DATA_SLICE } from "@/src/services/gql/mutations/data-browser";
 import { ModalEnum } from "@/src/types/shared/modal";
 import { getRawFilterForSave, parseFilterToExtended } from "@/src/util/components/projects/projectId/data-browser/filter-parser-helper";
 import { TOOLTIPS_DICT } from "@/src/util/tooltip-constants";
 import { SearchGroup, Slice } from "@/submodules/javascript-functions/enums/enums";
-import { useMutation } from "@apollo/client";
 import { Tooltip } from "@nextui-org/react";
 import { IconChartBubble, IconFilter, IconRotate } from "@tabler/icons-react";
 import { useDispatch, useSelector } from "react-redux";
@@ -30,8 +28,6 @@ export function DataSliceOperations(props: { fullSearch: {} }) {
     const additionalData = useSelector(selectAdditionalData);
     const embeddings = useSelector(selectEmbeddings);
 
-    const [updateDataSliceMut] = useMutation(UPDATE_DATA_SLICE);
-
     function updateSlice() {
         updateDataSlice(projectId, {
             static: activeSlice.static,
@@ -41,17 +37,6 @@ export function DataSliceOperations(props: { fullSearch: {} }) {
         }, (res) => {
             dispatch(setActiveDataSlice(activeSlice));
         });
-        // updateDataSliceMut({
-        //     variables: {
-        //         projectId: projectId,
-        //         static: activeSlice.static,
-        //         dataSliceId: activeSlice.id,
-        //         filterRaw: getRawFilterForSave(props.fullSearch),
-        //         filterData: parseFilterToExtended(activeSearchParams, attributes, configuration, labelingTasks, user, props.fullSearch[SearchGroup.DRILL_DOWN].value)
-        //     }
-        // }).then((res) => {
-        //     dispatch(setActiveDataSlice(activeSlice));
-        // });
         dispatch(updateAdditionalDataState('displayOutdatedWarning', false));
     }
 
