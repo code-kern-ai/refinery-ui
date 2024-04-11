03a4820afcdfce553469e6dca29d3db46b3131af
JWittmeyer <jens.wittmeyer@kern.ai>
2024-04-11 10:24:08 +0200

    Resolve merge issue

--- src/util/components/projects/projectId/model-callbacks-helper.ts
+++ src/util/components/projects/projectId/model-callbacks-helper.ts
@@ -3,9 +3,9 @@ import { getColorStruct, mapInformationSourceStatsGlobal } from "./heuristics/sh
 
 export const ACTIONS_DROPDOWN_OPTIONS = ['Select all', 'Deselect all', 'Delete selected'];
 
-export function postProcessModelCallbacks(modelCallBack: string) {
+export function postProcessModelCallbacks(modelCallBack: any[]) {
     if (!modelCallBack) return [];
-    return JSON.parse(modelCallBack).map((source) => {
+    return modelCallBack.map((source) => {
         source.labelSource = LabelSource.INFORMATION_SOURCE;
         source.stats = mapInformationSourceStatsGlobal(source.stat_data);
         source.stats.forEach((stat) => {
