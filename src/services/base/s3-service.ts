16241c5f6b52c700f86d3c745bfad847104a48e5
lumburovskalina <104008550+lumburovskalina@users.noreply.github.com>
2024-11-07 15:25:35 +0100

    new parent image (#50)
    * new parent image
    
    * Aws-sdk upload and download fix to use newest version

--- src/services/base/s3-service.ts
+++ src/services/base/s3-service.ts
@@ -1,7 +1,7 @@
 import { UploadState, UploadStates } from "@/src/types/shared/upload";
-import { S3 } from 'aws-sdk';
 import { ConfigManager } from "./config";
 import { Observable } from "rxjs";
+import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
 
 export function uploadFile(credentialsAndUploadIdParsed: any, file: File, filename: string): Observable<UploadState> {
@@ -11,42 +11,33 @@ export function uploadFile(credentialsAndUploadIdParsed: any, file: File, filena
     const s3Endpoint = ConfigManager.getConfigValue("KERN_S3_ENDPOINT");
     const s3Region = ConfigManager.getConfigValue('s3_region');
 
-    const s3Client = new S3({
+    const s3Client = new S3Client({
         endpoint: s3Endpoint,
-        accessKeyId: credentials["AccessKeyId"],
-        secretAccessKey: credentials["SecretAccessKey"],
-        sessionToken: credentials["SessionToken"],
         region: s3Region,
-        s3BucketEndpoint: false,
-        s3ForcePathStyle: true,
-        signatureVersion: 'v4',
-        sslEnabled: false,
+        credentials: {
+            accessKeyId: credentials["AccessKeyId"],
+            secretAccessKey: credentials["SecretAccessKey"],
+            sessionToken: credentials["SessionToken"],
+        },
     });
 
     const key = uploadTaskId + "/" + filename;
 
     return new Observable((subscriber) => {
-        var managedUpload = s3Client.upload({
+        s3Client.send(new PutObjectCommand({
             Bucket: bucket,
             Key: key,
             Body: file,
-        }, {}, function (err, data) {
-            if (err) {
-                subscriber.error({
-                    state: UploadStates.ERROR,
-                    progress: 100
-                })
-            }
+        })).then(data => {
             subscriber.next({
                 state: UploadStates.DONE,
                 progress: 100
-            })
-        });
-        return managedUpload.on('httpUploadProgress', function (progress) {
-            subscriber.next({
-                state: UploadStates.IN_PROGRESS,
-                progress: Math.round(progress.loaded / progress.total * 100)
-            })
+            });
+        }).catch(err => {
+            subscriber.error({
+                state: UploadStates.ERROR,
+                progress: 100
+            });
         });
     })
 }
@@ -57,17 +48,16 @@ export function downloadFile(credentialBlock: any, isStringData: boolean = true)
     const object = credentialBlock["objectName"];
     const bucket = credentialBlock["bucket"];
     const s3Endpoint = ConfigManager.getConfigValue("KERN_S3_ENDPOINT");
-    const s3Region = ConfigManager.getConfigValue('S3_REGION');
+    const s3Region = ConfigManager.getConfigValue('s3_region');
 
-    const s3Client = new S3({
+    const s3Client = new S3Client({
         endpoint: s3Endpoint,
-        accessKeyId: credentials["AccessKeyId"],
-        secretAccessKey: credentials["SecretAccessKey"],
-        sessionToken: credentials["SessionToken"],
         region: s3Region,
-        s3BucketEndpoint: false,
-        s3ForcePathStyle: true,
-        signatureVersion: 'v4',
+        credentials: {
+            accessKeyId: credentials["AccessKeyId"],
+            secretAccessKey: credentials["SecretAccessKey"],
+            sessionToken: credentials["SessionToken"],
+        },
     });
 
     var getParams = {
@@ -75,16 +65,19 @@ export function downloadFile(credentialBlock: any, isStringData: boolean = true)
         Key: object
     }
     return new Observable((subscriber) => {
-        s3Client.getObject(getParams, function (err, data) {
-            if (err) {
-                subscriber.error(null)
-            }
-            if (isStringData) {
-                let objectData = data.Body.toString('utf-8'); // Use the encoding necessary
-                subscriber.next(objectData)
+        s3Client.send(new GetObjectCommand(getParams)).then(data => {
+            const bodyContents = data.Body;
+            if (isStringData && bodyContents instanceof ReadableStream) {
+                const reader = bodyContents.getReader();
+                reader.read().then(({ done, value }) => {
+                    const objectData = new TextDecoder('utf-8').decode(value);
+                    subscriber.next(objectData);
+                });
             } else {
-                subscriber.next(data.Body)
+                subscriber.next(bodyContents);
             }
+        }).catch(err => {
+            subscriber.error(null);
         });
     });
 
