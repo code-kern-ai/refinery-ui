428112df580e2dc0d105f0a8ca6a679ead476184
anmarhindi <anmar.hindi@kern.ai>
2024-11-08 14:23:06 +0100

    reroute to base config default

--- src/services/base/config.ts
+++ src/services/base/config.ts
@@ -1,11 +1,12 @@
 import { FetchType, jsonFetchWrapper } from "@/submodules/javascript-functions/basic-fetch";
+import { BACKEND_BASE_URI } from "./_settings";
 
 export class ConfigManager {
 
     private static config = null;
 
     public static refreshConfig() {
-        jsonFetchWrapper('/config/base_config', FetchType.GET, r => {
+        jsonFetchWrapper(`${BACKEND_BASE_URI}/api/v1/misc/base-config-default`, FetchType.GET, r => {
             ConfigManager.config = r;
         });
     }
