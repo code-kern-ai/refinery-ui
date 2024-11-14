a2870f6a1f1ede1ee0a08f85cddddabac91d8fb1
JWittmeyer <jens.wittmeyer@kern.ai>
2024-11-14 11:19:24 +0100

    New url & submodule

--- src/services/base/config.ts
+++ src/services/base/config.ts
@@ -1,12 +1,11 @@
 import { FetchType, jsonFetchWrapper } from "@/submodules/javascript-functions/basic-fetch";
-import { BACKEND_BASE_URI } from "./_settings";
 
 export class ConfigManager {
 
     private static config = null;
 
     public static refreshConfig() {
-        jsonFetchWrapper(`${BACKEND_BASE_URI}/api/v1/misc/base-config-default`, FetchType.GET, r => {
+        jsonFetchWrapper(`/config/full_config`, FetchType.GET, r => {
             ConfigManager.config = r;
         });
     }
