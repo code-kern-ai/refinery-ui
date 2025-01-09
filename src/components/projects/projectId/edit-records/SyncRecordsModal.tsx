2569101b9e53ee0b42db3a9c60b3824526148327
anmarhindi <anmar.hindi@kern.ai>
2025-01-09 13:33:43 +0100

    Adjust response consumption for syncEditedRecords

--- src/components/projects/projectId/edit-records/SyncRecordsModal.tsx
+++ src/components/projects/projectId/edit-records/SyncRecordsModal.tsx
@@ -28,13 +28,13 @@ export default function SyncRecordsModal(props: SyncRecordsModalProps) {
         erdDataCopy.errors = null;
         const changes = jsonCopy(erdDataCopy.cachedRecordChanges);
         for (const key in changes) delete changes[key].display;
-        syncEditedRecords(projectId, changes, (tmp) => {
-            if (tmp?.ok) {
+        syncEditedRecords(projectId, changes, (res) => {
+            if (res?.ok) {
                 erdDataCopy.data.records = jsonCopy(erdDataCopy.displayRecords);
                 erdDataCopy.cachedRecordChanges = {};
                 dispatch(setModalStates(ModalEnum.SYNC_RECORDS, { syncModalAmount: Object.keys(erdDataCopy.cachedRecordChanges).length }));
             } else {
-                if (tmp) erdDataCopy.errors = tmp.errors;
+                if (res) erdDataCopy.errors = JSON.parse(res);
                 else erdDataCopy.errors = ["Request didn't go through"];
             }
             setSyncing(false);
