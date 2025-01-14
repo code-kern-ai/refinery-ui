033eba2279c46b9d0a8baa959e68ea7916d6d6e2
Lina <lina.lumburovska@kern.ai>
2025-01-14 09:10:13 +0100

    Updated heuristics statistics with tooltips

--- src/components/projects/projectId/heuristics/heuristicId/shared/HeuristicStatistics.tsx
+++ src/components/projects/projectId/heuristics/heuristicId/shared/HeuristicStatistics.tsx
@@ -1,9 +1,6 @@
-import MultilineTooltipAutoContent from "@/src/components/shared/multilines-tooltip/MultilineTooltipAuto";
 import { selectHeuristic } from "@/src/reduxStore/states/pages/heuristics"
-import { Stat } from "@/src/types/components/projects/projectId/heuristics/heuristics";
 import { HEURISTICS_STATISTICS_TABLE_COLUMNS, prepareTableBodyHeuristicStatistics } from "@/src/util/table-preparations/heuristic-statistics";
 import KernTable from "@/submodules/react-components/components/kern-table/KernTable";
-import { Tooltip } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux"
 
@@ -23,97 +20,6 @@ export default function HeuristicStatistics() {
                 <div className="overflow-x-auto">
                     <div className="inline-block min-w-full py-2 align-middle">
                         <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                            {/* <table className="min-w-full border divide-y divide-gray-300">
-                                <thead className="bg-gray-50">
-                                    <tr>
-                                        <th scope="col"
-                                            className="py-2 pl-4 pr-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 sm:pl-6">
-                                            Label</th>
-                                        <th scope="col"
-                                            className="px-3 py-2 text-xs font-medium uppercase tracking-wide text-gray-500">
-                                            <div className="flex w-full justify-center">
-                                                <Tooltip
-                                                    content={<MultilineTooltipAutoContent tooltip="True positives / (True positives + False positives)\nfor the reference data you labeled" splitOn="\n" />}
-                                                    color="invert"
-                                                    placement="top">Est. Precision</Tooltip>
-                                            </div>
-                                        </th>
-                                        {currentHeuristic.returnType == 'YIELD' && <th scope="col"
-                                            className="px-3 py-2 text-xs font-medium uppercase tracking-wide text-gray-500">
-                                            <div className="flex w-full justify-center">
-                                                <Tooltip
-                                                    content={<MultilineTooltipAutoContent tooltip="True positives / (True positives + False negatives)\nfor the reference data you labeled" splitOn="\n" />}
-                                                    color="invert"
-                                                    placement="top">Est. Recall</Tooltip>
-                                            </div>
-                                        </th>}
-                                        <th scope="col"
-                                            className="px-3 py-2 text-xs font-medium uppercase tracking-wide text-gray-500">
-                                            <div className="flex w-full justify-center">
-                                                <Tooltip
-                                                    content={<MultilineTooltipAutoContent tooltip="How many records does this\nheuristic generally hit?" splitOn="\n" />}
-                                                    color="invert"
-                                                    placement="top">Coverage</Tooltip>
-                                            </div>
-                                        </th>
-                                        <th scope="col"
-                                            className="px-3 py-2 text-xs font-medium uppercase tracking-wide text-gray-500">
-                                            <div className="flex w-full justify-center">
-                                                <Tooltip
-                                                    content={<MultilineTooltipAutoContent tooltip="How many spans are hit\nby this heuristic?" splitOn="\n" />}
-                                                    color="invert"
-                                                    placement="top">Hits</Tooltip>
-                                            </div>
-                                        </th>
-                                        <th scope="col"
-                                            className="px-3 py-2 text-xs font-medium uppercase tracking-wide text-gray-500">
-                                            <div className="flex w-full justify-center">
-                                                <Tooltip
-                                                    content={<MultilineTooltipAutoContent tooltip="On how many records (or spans)\ndoes this heuristic create conflicting\nexpressions to other heuristics?" splitOn="\n" />}
-                                                    color="invert"
-                                                    placement="top">Conflicts</Tooltip>
-                                            </div>
-                                        </th>
-                                        <th scope="col"
-                                            className="px-3 py-2 text-xs font-medium uppercase tracking-wide text-gray-500">
-                                            <div className="flex w-full justify-center">
-                                                <Tooltip
-                                                    content={<MultilineTooltipAutoContent tooltip="On how many records (or spans)\ndoes this heuristic create overlapping\nexpressions to other heuristics?" splitOn="\n" />}
-                                                    color="invert"
-                                                    placement="top">Overlaps</Tooltip>
-                                            </div>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody className="divide-y divide-gray-200">
-                                    {currentHeuristic.stats.map((sourceContainer: Stat, j) => (<tr key={sourceContainer.label} className={`${j % 2 != 0 ? 'bg-gray-50' : 'bg-white'}`}>
-                                        <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                                            <span className={`inline-flex items-center px-2 py-0.5 rounded text-xs font-medium border ${sourceContainer.color.backgroundColor} ${sourceContainer.color.textColor} ${sourceContainer.color.borderColor}`}>
-                                                {sourceContainer.label}
-                                            </span>
-                                        </td>
-                                        <td className="whitespace-nowrap text-center px-3 py-2 text-sm text-gray-500">
-                                            {sourceContainer.values.Precision}
-                                        </td>
-                                        {currentHeuristic.returnType == 'YIELD' && <td className="whitespace-nowrap text-center px-3 py-2 text-sm text-gray-500">
-                                            {sourceContainer.values.Recall}
-                                        </td>}
-                                        <td className="whitespace-nowrap text-center px-3 py-2 text-sm text-gray-500">
-                                            {sourceContainer.values.Coverage}
-                                        </td>
-                                        <td className="whitespace-nowrap text-center px-3 py-2 text-sm text-gray-500">
-                                            {sourceContainer.values.TotalHits}
-                                        </td>
-                                        <td className="whitespace-nowrap text-center px-3 py-2 text-sm text-gray-500">
-                                            {sourceContainer.values.Conflicts}
-                                        </td>
-                                        <td className="whitespace-nowrap text-center px-3 py-2 text-sm text-gray-500">
-                                            {sourceContainer.values.Overlaps}
-                                        </td>
-                                    </tr>))}
-                                </tbody>
-                            </table> */}
-
                             <KernTable
                                 headers={HEURISTICS_STATISTICS_TABLE_COLUMNS}
                                 values={preparedValues}
--- src/util/table-preparations/heuristic-statistics.ts
+++ src/util/table-preparations/heuristic-statistics.ts
@@ -1,4 +1,11 @@
-export const HEURISTICS_STATISTICS_TABLE_COLUMNS = [{ column: 'Label', id: 'label' }, { column: 'Est. Precision', id: 'precision' }, { column: 'Est. Recall', id: 'recall' }, { column: 'Coverage', id: 'coverage' }, { column: 'Hits', id: 'hits' }, { column: 'Conflicts', id: 'conflicts' }, { column: 'Overlaps', id: 'overlaps' }];
+export const HEURISTICS_STATISTICS_TABLE_COLUMNS = [
+    { column: 'Label', id: 'label' },
+    { column: 'Est. Precision', id: 'precision', tooltip: 'True positives / (True positives + False positives)\nfor the reference data you labeled' },
+    { column: 'Est. Recall', id: 'recall', tooltip: 'True positives / (True positives + False negatives)\nfor the reference data you labeled' },
+    { column: 'Coverage', id: 'coverage', tooltip: 'How many records does this\nheuristic generally hit?' },
+    { column: 'Hits', id: 'hits', tooltip: 'How many spans are hit\nby this heuristic?' },
+    { column: 'Conflicts', id: 'conflicts', tooltip: 'On how many records (or spans)\ndoes this heuristic create conflicting\nexpressions to other heuristics?' },
+    { column: 'Overlaps', id: 'overlaps', tooltip: 'On how many records (or spans)\ndoes this heuristic create overlapping\nexpressions to other heuristics?' }];
 
 export function prepareTableBodyHeuristicStatistics(stats) {
     let finalData = [];
