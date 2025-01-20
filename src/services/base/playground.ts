18bc201a9c873ce3e0cd41c4951f1034b1f44642
Lina <lina.lumburovska@kern.ai>
2025-01-20 10:50:43 +0100

    Build fix

--- src/services/base/playground.ts
+++ src/services/base/playground.ts
@@ -3,7 +3,7 @@ import { BACKEND_BASE_URI } from "./_settings";
 
 export const playgroundEndpoint = `${BACKEND_BASE_URI}/api/v1/playground`;
 
-export function getSearchResults(projectId: string, embeddingId: string, question: string, limit: Int16Array, onResult: (result: any) => void) {
+export function getSearchResults(projectId: string, embeddingId: string, question: string, limit: number, onResult: (result: any) => void) {
     const finalUrl = `${playgroundEndpoint}/${projectId}/search`;
     const body = {
         embeddingId: embeddingId,
