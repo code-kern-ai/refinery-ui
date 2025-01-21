e85d567ff307e94bce25ae1a32d093f6918bcddd
Lina <lina.lumburovska@kern.ai>
2025-01-21 16:09:27 +0100

    simple display for evaluation run details page

--- src/services/base/playground.ts
+++ src/services/base/playground.ts
@@ -82,3 +82,8 @@ export function createEvaluationRun(projectId, embeddingId: string, evaluationGr
     }
     jsonFetchWrapper(finalUrl, FetchType.POST, onResult, JSON.stringify(body));
 }
+
+export function getEvaluationRunById(projectId: string, evaluationRunId: string, onResult: (result: any) => void) {
+    const finalUrl = `${playgroundEndpoint}/${projectId}/evaluation-runs/${evaluationRunId}`;
+    jsonFetchWrapper(finalUrl, FetchType.GET, onResult);
+}
