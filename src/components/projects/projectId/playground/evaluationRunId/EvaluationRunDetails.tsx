acda7951212c56109f2cd883c20b541829c2dbe1
Lina <lina.lumburovska@kern.ai>
2025-01-23 12:15:50 +0100

    Added statistics per set and toggling show hide records

--- src/components/projects/projectId/playground/evaluationRunId/EvaluationRunDetails.tsx
+++ src/components/projects/projectId/playground/evaluationRunId/EvaluationRunDetails.tsx
@@ -1,24 +1,27 @@
 import { RecordDisplay } from "@/src/components/shared/record-display/RecordDisplay"
-import { selectVisibleAttributesHeuristics } from "@/src/reduxStore/states/pages/settings"
+import { selectVisibleAttributesHeuristics, setAllAttributes } from "@/src/reduxStore/states/pages/settings"
 import { selectProjectId } from "@/src/reduxStore/states/project"
+import { getAttributes } from "@/src/services/base/attribute"
 import { getEvaluationRunById, getEvaluationSets } from "@/src/services/base/playground"
-import { EVALUATION_RUN_DETAILS_TABLE_HEADER, prepareEvaluationRunDetailsTableBody } from "@/src/util/table-preparations/evaluation-run-details"
-import { arrayToDict } from "@/submodules/javascript-functions/general"
-import KernTable from "@/submodules/react-components/components/kern-table/KernTable"
+import { arrayToDict, percentRoundString } from "@/submodules/javascript-functions/general"
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
+import { Fragment, useEffect, useMemo, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
 
 export default function EvaluationRunDetails() {
     const router = useRouter()
+    const dispatch = useDispatch();
 
     const projectId = useSelector(selectProjectId);
     const attributes = useSelector(selectVisibleAttributesHeuristics);
 
-
     const [evaluationRun, setEvaluationRun] = useState(null)
     const [evaluationSetsDict, setEvaluationSetsDict] = useState(null)
-    const [preparedValues, setPreparedValues] = useState([])
+
+    useEffect(() => {
+        if (!projectId) return;
+        refetchAttributesAndProcess();
+    }, [projectId]);
 
     useEffect(() => {
         if (!projectId || !router.query.evaluationRunId) return;
@@ -30,11 +33,11 @@ export default function EvaluationRunDetails() {
         })
     }, [projectId, router.query.evaluationRunId])
 
-    useEffect(() => {
-        if (!evaluationRun || !evaluationSetsDict) return;
-        // TODO: send the correct number of selected records
-        setPreparedValues(prepareEvaluationRunDetailsTableBody(evaluationRun.results, 10))
-    }, [evaluationRun, evaluationSetsDict])
+    function refetchAttributesAndProcess() {
+        getAttributes(projectId, ['ALL'], (res) => {
+            dispatch(setAllAttributes(res));
+        });
+    }
 
     return <>
         {projectId && <div className={`grid overflow-hidden min-h-full p-4`}>
@@ -45,38 +48,41 @@ export default function EvaluationRunDetails() {
                 </div>
             </div>
             {evaluationRun && evaluationSetsDict && <div>
-                {evaluationRun.results.map((result, index) => <>
-                    <div className="text-md leading-5 font-normal text-gray-500 inline-block my-5"><strong>Question:</strong> {evaluationSetsDict[result.evaluationSetId].question}</div>
-
-                    <KernTable
-                        headers={EVALUATION_RUN_DETAILS_TABLE_HEADER}
-                        values={preparedValues}
-                    />
-
+                {evaluationRun.results.map((result, index) => <Fragment key={index}>
+                    <div className="text-md leading-5 font-normal text-gray-500 my-5"><strong>Question:</strong> {evaluationSetsDict[result.evaluationSetId].question}</div>
                     <div className="grid grid-cols-3 gap-x-2 border border-gray-300 rounded-md p-2">
-                        <RecordDisplaySearches attributes={attributes} records={result.truePositives} text="True positives" />
-                        <RecordDisplaySearches attributes={attributes} records={result.falsePositives} text="False positives" />
-                        <RecordDisplaySearches attributes={attributes} records={result.falseNegatives} text="False negatives" />
+                        <RecordDisplaySearches attributes={attributes} records={result.truePositives} text="True positives" recordsInSet={evaluationSetsDict[result.evaluationSetId].recordIds.length} />
+                        <RecordDisplaySearches attributes={attributes} records={result.falsePositives} text="False positives" recordsInSet={evaluationSetsDict[result.evaluationSetId].recordIds.length} />
+                        <RecordDisplaySearches attributes={attributes} records={result.falseNegatives} text="False negatives" recordsInSet={evaluationSetsDict[result.evaluationSetId].recordIds.length} />
                     </div>
-                </>)}
+                </Fragment>)}
             </div >}
         </div>
         }
     </>
 }
 
-function RecordDisplaySearches({ attributes, records, text }) {
+function RecordDisplaySearches({ attributes, records, text, recordsInSet }) {
+    const calculatePercentage = useMemo(() => percentRoundString(records.length / recordsInSet), [records])
+    const [showRecords, setShowRecords] = useState(false)
+
     return <div>
-        <div className="text-center my-2">{text}</div>
-        {records.length > 0 ? <>
-            {records.map((record, index) => <div key={record.id} className="my-2">
-                <div key={index} className="flex flex-col gap-x-3 bg-white rounded-md border border-gray-300 py-2 px-3 m-2">
-                    <RecordDisplay
-                        key={index}
-                        attributes={attributes}
-                        record={record} />
-                </div>
-            </div>)}
-        </> : <div className="leading-5 text-sm font-normal-text-gray-500 text-gray-500 italic">There are no matching results</div>}
+        <div className="flex flex-col gap-y-3 justify-center items-center">
+            <div className="text-center my-2">{text} {calculatePercentage}</div>
+            <button className="text-sm font-normal text-gray-500 underline" onClick={() => setShowRecords(!showRecords)}>{showRecords ? "Hide" : "Show"} records</button>
+        </div>
+
+        {showRecords && <>
+            {records.length > 0 ? <>
+                {records.map((record, index) => <div key={record.id} className="my-2">
+                    <div key={index} className="flex flex-col gap-x-3 bg-white rounded-md border border-gray-300 py-2 px-3 m-2">
+                        <RecordDisplay
+                            key={index}
+                            attributes={attributes}
+                            record={record} />
+                    </div>
+                </div>)}
+            </> : <div className="leading-5 text-sm font-normal-text-gray-500 text-gray-500 italic">There are no matching results</div>}
+        </>}
     </div>
 }
