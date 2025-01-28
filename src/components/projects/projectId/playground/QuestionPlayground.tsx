a99710fe652f34acf9f2a9301499106baf0811ad
Lina <lina.lumburovska@kern.ai>
2025-01-28 15:52:06 +0100

    Navigation improvements

--- src/components/projects/projectId/playground/QuestionPlayground.tsx
+++ src/components/projects/projectId/playground/QuestionPlayground.tsx
@@ -1,7 +1,6 @@
 import { selectProjectId } from "@/src/reduxStore/states/project"
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
-import style from '@/src/styles/components/projects/projectId/playground.module.css';
 import { PlaygroundSearch } from "./PlaygroundSearch";
 import { EvaluationSets } from "./EvaluationSets";
 import { EvaluationGroups } from "./EvaluationGroups";
@@ -10,8 +9,14 @@ import { getAttributes } from "@/src/services/base/attribute";
 import { setAllAttributes, setAllEmbeddings } from "@/src/reduxStore/states/pages/settings";
 import { getEmbeddings } from "@/src/services/base/embedding";
 import { postProcessingEmbeddings } from "@/src/util/components/projects/projectId/settings/embeddings-helper";
+import { IconCategoryPlus } from "@tabler/icons-react";
 
-const PLAYGROUND_TABS = ['Playground', 'Sets', 'Groups', 'Runs'];
+const STEPS = [
+    { id: 0, name: 'Playground' },
+    { id: 1, name: 'Sets' },
+    { id: 2, name: 'Groups' },
+    { id: 3, name: 'Runs' },
+]
 
 export default function QuestionPlayground() {
     const dispatch = useDispatch();
@@ -44,13 +49,69 @@ export default function QuestionPlayground() {
     }
 
     return <>
-        {projectId && <><div className={`grid overflow-hidden min-h-full p-4`}>
-            <div className={`flex max-w-full overflow-x-auto border-b-2 border-b-gray-200`}>
-                {PLAYGROUND_TABS.map((tab, index) => <div key={tab}>
-                    <div className={`cursor-pointer text-sm leading-5 font-medium mr-10 py-5 ${openTab == index ? 'text-indigo-700 ' + style.borderBottom : 'text-gray-500'}`} onClick={() => toggleTabs(index)}>{tab}</div>
-                </div>)}
+        {projectId && <>
+            <div className={`overflow-hidden min-h-full p-4 flex items-center`}>
+                <nav aria-label="Progress" className="flex items-center w-full">
+                    <ol role="list" className="divide-y divide-gray-300 rounded-md border border-gray-300 md:flex md:divide-y-0">
+                        <li className="relative md:flex md:flex-1 cursor-pointer" onClick={() => toggleTabs(0)}>
+                            {openTab == 0 ? (
+                                <div className="flex items-center px-6 py-2 text-sm font-medium" aria-current="step">
+                                    <span className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border-2 border-purple-600">
+                                        <IconCategoryPlus className="text-purple-600 h-5 w-5" />
+                                    </span>
+                                    <span className="ml-4 text-sm font-medium text-purple-600">{STEPS[0].name}</span>
+                                </div>
+                            ) : (
+                                <div className="group flex items-center">
+                                    <span className="flex items-center px-6 py-2 text-sm font-medium">
+                                        <span className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border-2 border-gray-300">
+                                            <IconCategoryPlus className="text-gray-500 h-5 w-5" />
+                                        </span>
+                                        <span className="ml-4 text-sm font-medium text-gray-500">{STEPS[0].name}</span>
+                                    </span>
+                                </div>
+                            )}
+                        </li>
+                    </ol>
+                    <ol className="divide-y divide-gray-300 rounded-md border border-gray-300 md:flex md:divide-y-0 ml-auto">
+                        {STEPS.slice(1).map((step, stepIdx) => (
+                            <li key={step.name} className="relative md:flex md:flex-1 cursor-pointer" onClick={() => toggleTabs(stepIdx + 1)}>
+                                {openTab == step.id ? (
+                                    <div className="flex items-center px-6 py-2 text-sm font-medium" aria-current="step">
+                                        <span className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border-2 border-purple-600">
+                                            <span className="text-purple-600">{step.id}</span>
+                                        </span>
+                                        <span className="ml-4 text-sm font-medium text-purple-600">{step.name}</span>
+                                    </div>
+                                ) : (
+                                    <div className="group flex items-center">
+                                        <span className="flex items-center px-6 py-2 text-sm font-medium">
+                                            <span className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border-2 border-gray-300">
+                                                <span className="text-gray-500">{step.id}</span>
+                                            </span>
+                                            <span className="ml-4 text-sm font-medium text-gray-500">{step.name}</span>
+                                        </span>
+                                    </div>
+                                )}
+                                {stepIdx !== STEPS.length - 2 ? (
+                                    <>
+                                        <div className="absolute right-0 top-0 hidden h-full w-3 md:block" aria-hidden="true">
+                                            <svg
+                                                className="h-full w-full text-gray-300"
+                                                viewBox="0 0 12 82"
+                                                fill="none"
+                                                preserveAspectRatio="none"
+                                            >
+                                                <path d="M0.5 0V31L10.5 41L0.5 51V82" stroke="currentcolor" vectorEffect="non-scaling-stroke" />
+                                            </svg>
+                                        </div>
+                                    </>
+                                ) : null}
+                            </li>
+                        ))}
+                    </ol>
+                </nav>
             </div>
-        </div>
             {openTab === 0 && <PlaygroundSearch />}
             {openTab === 1 && <EvaluationSets />}
             {openTab === 2 && <EvaluationGroups />}
