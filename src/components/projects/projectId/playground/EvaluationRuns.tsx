a67b54259a46a0cfa1b0b6df07767ffd54f8becb
LennartSchmidtKern <lennart.schmidt@kern.ai>
2025-01-28 10:57:29 +0100

    fix dynamici height

--- src/components/projects/projectId/playground/EvaluationRuns.tsx
+++ src/components/projects/projectId/playground/EvaluationRuns.tsx
@@ -106,7 +106,7 @@ export default function EvaluationRuns() {
                 </div>
                 <button
                     className={`ml-auto bg-green-100 border border-green-400 text-green-700 text-xs font-semibold px-4 py-2 rounded-md cursor-pointer opacity-100 hover:bg-green-200 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50`}
-                    onClick={() => dispatch(openModal(ModalEnum.EVALUATION_RUN))}>Create evaluation run</button>
+                    onClick={() => dispatch(openModal(ModalEnum.EVALUATION_RUN))}>Run Evaluation</button>
             </div >
             {preparedHeaders && preparedValues && (evaluationRuns.length > 0 ?
                 <KernTable
