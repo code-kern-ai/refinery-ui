9a657754e0ef5aa2c45cf3568882f6a5aa72296f
Lina <lina.lumburovska@kern.ai>
2025-02-03 09:20:58 +0100

    Question improvement modal visuals

--- src/components/projects/projectId/playground/PlaygroundSearchReformulateModal.tsx
+++ src/components/projects/projectId/playground/PlaygroundSearchReformulateModal.tsx
@@ -17,14 +17,13 @@ export default function PlaygroundSearchReformulateModal(props: { handleReformul
 
     return (
         <Modal modalName={ModalEnum.EVALUATION_REFORMULATE} acceptButton={acceptButton} className="w-80">
-            <h3 className="text-lg font-semibold mb-2">Question Improvement</h3>
-            <p> Generate an LLM-assisted reformulation of your question to better align with your search intent and improve the relevance of the results.</p>
-            <input
+            <h3 className="ext-lg text-gray-900 font-medium mb-2">Question Improvement</h3>
+            <div className="text-sm text-gray-500 my-2">Generate an LLM-assisted reformulation of your question to better align with your search intent and improve the relevance of the results.</div>            <input
                 type="text"
                 value={apiKey}
                 onChange={(e) => setApiKey(e.target.value)}
                 placeholder="OpenAI API key"
-                className="w-full p-2 mt-3 border rounded"
+                className="w-full p-2 mt-3 border h-9 text-sm border-gray-300 rounded-md placeholder-italic text-gray-900 pl-4 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2 focus:ring-offset-gray-100"
             />
         </Modal>
     );
