cbc1a5df7ca7b113e570f717003715e34f3ad775
anmarhindi <153752112+anmarhindi@users.noreply.github.com>
2025-03-20 12:25:10 +0100

    v1.19.0 (#69)
    * Initial commit
    
    * Fixes same embedding name for openai & azure check
    
    * Fixes padding on question playground results
    
    * Updates project record count on upload
    
    * Adds border to no logs for ac calc
    
    * sub ref
    
    ---------
    
    Co-authored-by: JWittmeyer <jens.wittmeyer@kern.ai>

--- src/pages/projects/[projectId]/upload-records/index.tsx
+++ src/pages/projects/[projectId]/upload-records/index.tsx
@@ -1,16 +1,34 @@
 import UploadRecords from "@/src/components/projects/projectId/upload-records/UploadRecords";
-import { setCurrentPage, setDisplayIconComments } from "@/src/reduxStore/states/general";
-import { CurrentPage } from "@/submodules/react-components/hooks/web-socket/constants";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux"
+import { selectOrganizationId, setCurrentPage, setDisplayIconComments } from "@/src/reduxStore/states/general";
+import { selectProjectId, setActiveProject } from "@/src/reduxStore/states/project";
+import { getProjectByProjectId } from "@/src/services/base/project";
+import { Application, CurrentPage, CurrentPageSubKey } from "@/submodules/react-components/hooks/web-socket/constants";
+import { useWebsocket } from "@/submodules/react-components/hooks/web-socket/useWebsocket";
+import { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
 
 export default function UploadRecordsPage() {
     const dispatch = useDispatch();
+    const projectId = useSelector(selectProjectId);
+    const orgId = useSelector(selectOrganizationId);
 
     useEffect(() => {
         dispatch(setCurrentPage(CurrentPage.UPLOAD_RECORDS));
         dispatch(setDisplayIconComments(false));
     }, []);
 
-    return (<UploadRecords></UploadRecords>)
+
+    const handleWebsocketNotification = useCallback((msgParts: string[]) => {
+        if (!projectId) return;
+        if (msgParts[1] == 'project_update' && msgParts[2] == projectId) {
+            getProjectByProjectId(projectId, (res) => {
+                dispatch(setActiveProject(res));
+            })
+        }
+
+    }, [projectId]);
+
+    useWebsocket(orgId, Application.REFINERY, CurrentPage.PROJECT_SETTINGS, handleWebsocketNotification, undefined, CurrentPageSubKey.FILE_UPLOAD);
+
+    return <UploadRecords />
 }
