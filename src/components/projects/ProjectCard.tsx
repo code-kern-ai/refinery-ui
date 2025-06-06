ed29ed134aaeedef0bf992212a663051ac2aa338
Lina <lina.lumburovska@kern.ai>
2025-06-06 13:00:36 +0200

    Added  warning message for integration projects

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -65,6 +65,9 @@ export default function ProjectCard(props: ProjectCardProps) {
                             <span className="text-sm text-gray-900 ">{props.project.time}</span>
                         </>}
                     </div>}
+                    {props.project.is_integration_project && <div className="absolute bottom-0 left-2/4 flex flex-row flex-nowrap gap-x-1 bg-gray-100 px-1 rounded-br rounded-bl" style={{ 'transform': 'translate(-50%' }}>
+                        <span className="text-sm text-red-500">This is an integration project, please do not delete it unless the integration is deleted</span>
+                    </div>}
                     {(isAdmin && props.project.status !== ProjectStatus.INIT_SAMPLE_PROJECT) &&
                         <div className="absolute top-0 left-0 cursor-pointer" onClick={adminOpenOrDeleteProjectFunc}>
                             <Tooltip content={TOOLTIPS_DICT.PROJECTS.QUICK_DELETE} color="invert" offset={2} placement="right">
