7efed50475488f5950738732e93cfe788bbd6ba6
Lina <lina.lumburovska@kern.ai>
2025-07-02 14:23:37 +0200

    new parent image

--- src/services/base/s3-service.ts
+++ src/services/base/s3-service.ts
@@ -44,7 +44,7 @@ export function uploadFile(credentialsAndUploadIdParsed: any, file: File, filena
 }
 
 export function downloadFile(credentialBlock: any, isStringData: boolean = true): any {
-
+    console.log(credentialBlock)
     const credentials = credentialBlock["Credentials"];
     const object = credentialBlock["objectName"];
     const bucket = credentialBlock["bucket"];
