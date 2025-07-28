a1f411f90d1a198b2765ba6a3ddc96afe6ef4eed
JWittmeyer <jens.wittmeyer@kern.ai>
2025-07-28 13:23:38 +0200

    privatemode ai for ac

--- src/components/projects/projectId/attributes/attributeId/LLM/LLMConfig.tsx
+++ src/components/projects/projectId/attributes/attributeId/LLM/LLMConfig.tsx
@@ -3,6 +3,7 @@ import OpenAI from "./models/OpenAI"
 import Azure from "./models/Azure"
 import { LLMConfigProps } from "./types";
 import AzureFoundry from "./models/AzureFoundry";
+import PrivatemodeAi from "./models/PrivatemodeAi";
 
 export function LLMConfig(props: LLMConfigProps) {
     switch (props.llmIdentifier) {
@@ -12,6 +13,8 @@ export function LLMConfig(props: LLMConfigProps) {
             return <Azure disabled={props.disabled} llmConfig={props.llmConfig} setLlmConfig={props.setLlmConfig} onlyEssential={props.onlyEssential} projectId={props.projectId} />
         case 'Azure Foundry':
             return <AzureFoundry disabled={props.disabled} llmConfig={props.llmConfig} setLlmConfig={props.setLlmConfig} onlyEssential={props.onlyEssential} projectId={props.projectId} />
+        case 'Privatemode AI':
+            return <PrivatemodeAi disabled={props.disabled} llmConfig={props.llmConfig} setLlmConfig={props.setLlmConfig} onlyEssential={props.onlyEssential} projectId={props.projectId} />
         default:
             return null
     }
---
+++ src/components/projects/projectId/attributes/attributeId/LLM/models/PrivatemodeAi.tsx
@@ -0,0 +1,79 @@
+import { useEffect } from "react";
+import KernDropdown from "@/submodules/react-components/components/KernDropdown";
+import { InfoButton } from "@/submodules/react-components/components/InfoButton";
+import { InputWithSlider } from "@/submodules/react-components/components/InputWithSlider";
+import { LLmPropsPrivatemodeAI } from "../types";
+
+
+export const MODEL_MAP_FULL_NAME = {
+    'Meta-Llama-3.3-70B': "ibnzterrell/Meta-Llama-3.3-70B-Instruct-AWQ-INT4",
+    'gemma-3-27b': "leon-se/gemma-3-27b-it-fp8-dynamic",
+}
+const REVERSE_MAP_FULL_NAME = Object.fromEntries(Object.entries(MODEL_MAP_FULL_NAME).map(([key, value]) => [value, key]));
+export const MODEL_OPTIONS = Object.keys(MODEL_MAP_FULL_NAME);
+
+export default function PrivatemodeAi(props: LLmPropsPrivatemodeAI) {
+    useEffect(() => {
+        if (props.llmConfig.model && !MODEL_OPTIONS.includes(props.llmConfig.model)) {
+            props.setLlmConfig({ ...props.llmConfig, model: MODEL_MAP_FULL_NAME[MODEL_OPTIONS[0]] })
+        }
+    }, []);
+    return (
+        <div className='flex flex-col'>
+            {props.onlyEssential ?
+                null : <div className="mb-2 flex items-center">
+                    <span className="text-sm font-medium text-gray-900 line-through">API Key  </span>
+                    <InfoButton content="Set in backend" divPosition="right" infoButtonSize="sm" />
+                </div>}
+            <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900">Model</label>
+                <KernDropdown
+                    buttonName={props.llmConfig.model ? REVERSE_MAP_FULL_NAME[props.llmConfig.model] : 'Select model'}
+                    options={MODEL_OPTIONS}
+                    selectedOption={(option) => props.setLlmConfig({ ...props.llmConfig, model: MODEL_MAP_FULL_NAME[option] })}
+                    disabled={props.disabled}
+                />
+            </div>
+            {props.onlyEssential ? null : <>
+                <InputWithSlider
+                    label="Temperature"
+                    value={props.llmConfig.temperature}
+                    min={0}
+                    max={2}
+                    step={0.01}
+                    onChange={(value) => props.setLlmConfig({ ...props.llmConfig, temperature: value })}
+                    disabled={props.disabled}
+                />
+
+                <InputWithSlider
+                    label="Top P"
+                    value={props.llmConfig.topP}
+                    min={0}
+                    max={1}
+                    step={0.01}
+                    onChange={(value) => props.setLlmConfig({ ...props.llmConfig, topP: value })}
+                    disabled={props.disabled}
+                />
+                <InputWithSlider
+                    label='Frequency penalty'
+                    value={props.llmConfig.frequencyPenalty}
+                    min={-2}
+                    max={2}
+                    step={0.01}
+                    onChange={(value) => props.setLlmConfig({ ...props.llmConfig, frequencyPenalty: value })}
+                    disabled={props.disabled}
+                />
+                <InputWithSlider
+                    label='Presence penalty'
+                    value={props.llmConfig.presencePenalty}
+                    min={-2}
+                    max={2}
+                    step={0.01}
+                    onChange={(value) => props.setLlmConfig({ ...props.llmConfig, presencePenalty: value })}
+                    disabled={props.disabled}
+                />
+            </>}
+
+        </div>
+    )
+}
