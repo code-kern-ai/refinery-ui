0ec94e5d6dceb30e1252eecd02d555986472618f
andhreljaKern <andrea.hrelja@kern.ai>
2025-12-29 10:22:00 +0100

    perf: update knowledge graph routes

--- src/services/base/knowledge-graphs.ts
+++ src/services/base/knowledge-graphs.ts
@@ -1,14 +1,33 @@
-import { FetchType, jsonFetchWrapper } from "@/submodules/javascript-functions/basic-fetch";
 import { BACKEND_BASE_URI } from "./_settings";
+import { FetchType, jsonFetchWrapper } from "@/submodules/javascript-functions/basic-fetch";
+import { convertCamelToSnakeCase } from "@/submodules/javascript-functions/case-types-parser";
 
 export const knowledgeGraphEndpoint = `${BACKEND_BASE_URI}/api/v1/knowledge-graph`;
 
 export function deleteKnowledgeGraphById(projectId: string, knowledgeGraphId: string, onResult: (result: any) => void) {
-    const finalUrl = `${knowledgeGraphEndpoint}/${projectId}/${knowledgeGraphId}/delete-knowledge-graph`;
-    jsonFetchWrapper(finalUrl, FetchType.DELETE, onResult);
+    const finalUrl = `${knowledgeGraphEndpoint}/${projectId}`;
+    const body = {
+        ids: [knowledgeGraphId]
+    };
+    jsonFetchWrapper(finalUrl, FetchType.DELETE, onResult, JSON.stringify(convertCamelToSnakeCase(body)));
 }
 
 export function getKnowledgeGraphs(projectId: string, onResult: (result: any) => void) {
-    const finalUrl = `${knowledgeGraphEndpoint}/${projectId}/knowledge-graphs`;
+    const finalUrl = `${knowledgeGraphEndpoint}/project/${projectId}`;
     jsonFetchWrapper(finalUrl, FetchType.GET, onResult);
+}
+
+export function getKnowledgeGraph(knowledgeGraphId: string, onResult: (result: any) => void) {
+    const finalUrl = `${knowledgeGraphEndpoint}/${knowledgeGraphId}`;
+    jsonFetchWrapper(finalUrl, FetchType.GET, onResult);
+}
+
+export function createKnowledgeGraph(projectId: string, name: string, description: string, type: string, onResult: (result: any) => void) {
+    const body = {
+        projectId: projectId,
+        name: name,
+        description: description,
+        type: type
+    };
+    jsonFetchWrapper(knowledgeGraphEndpoint, FetchType.POST, onResult, JSON.stringify(convertCamelToSnakeCase(body)));
 }
