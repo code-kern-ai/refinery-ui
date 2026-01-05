95afc971b98b657ec12b95efb1e920ca4829f06b
Lina <lina.lumburovska@kern.ai>
2026-01-05 13:11:06 +0100

    Rework checkboxes on stable knowledge graphs

--- src/components/projects/projectId/knowledge-graphs/knowledgeGraphId/StableKnowledgeGraphDetailsOverview.tsx
+++ src/components/projects/projectId/knowledge-graphs/knowledgeGraphId/StableKnowledgeGraphDetailsOverview.tsx
@@ -1,11 +1,11 @@
 import { selectProjectId } from "@/src/reduxStore/states/project";
 import { getKnowledgeGraphsDataStable } from "@/src/services/base/knowledge-graphs";
 import { arrayToDict } from "@/submodules/javascript-functions/general";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import { useColumns } from "./useColumns";
 import { selectAllUsers } from "@/src/reduxStore/states/general";
-import { useConsoleLog } from "@/submodules/react-components/hooks/useConsoleLog";
+import KernDropdown from "@/submodules/react-components/components/KernDropdown";
 
 const VISIBLE_KEYS_INTEGRATIONS = ['createdBy', 'updatedBy', 'createdAt', 'updatedAt', 'runningId', 'errorMessage', 'extension', 'name', 'size', 'content'];
 const DATE_KEYS = ['createdAt', 'updatedAt', 'created', 'modified'];
@@ -16,11 +16,9 @@ export default function StableKnowledgeGraphDetailsOverview() {
     const projectId = useSelector(selectProjectId);
     const users = useSelector(selectAllUsers);
     const usersDict = arrayToDict(users, 'id');
-    useConsoleLog(usersDict)
 
     const [stableData, setStableData] = useState(null);
-    const [tableHeaders, setTableHeaders] = useState<string[]>([]);
-    const [visibleHeaders, setVisibleHeaders] = useState(new Set(VISIBLE_KEYS_INTEGRATIONS.filter(col => tableHeaders.includes(col))));
+    const [selectedHeaders, setSelectedHeaders] = useState<{ name: string, checked: boolean }[]>([]);
 
     useEffect(() => {
         if (!projectId) return;
@@ -29,27 +27,20 @@ export default function StableKnowledgeGraphDetailsOverview() {
 
     useEffect(() => {
         if (!stableData) return;
-        const keys = new Set<string>();
-        Object.keys(stableData[0]).forEach(key => {
-            if (typeof stableData[0][key] === 'object' && stableData[0][key] !== null) return;
-            keys.add(key)
-        });
-        setTableHeaders(Array.from(keys));
+        setSelectedHeaders(Object.keys(stableData[0]).map(key => ({ name: key, checked: VISIBLE_KEYS_INTEGRATIONS.includes(key) })));
     }, [stableData]);
 
-    useEffect(() => {
-        setVisibleHeaders(new Set(VISIBLE_KEYS_INTEGRATIONS.filter(col => tableHeaders.includes(col))));
-    }, [tableHeaders]);
+    const mappedHeadersByName = useMemo(() => {
+        return selectedHeaders.filter(h => h.checked).map(h => h.name);
+    }, [selectedHeaders]);
 
-    const toggleHeaders = useCallback((key: string) => {
-        setVisibleHeaders(prev => {
-            const next = new Set(prev);
-            next.has(key) ? next.delete(key) : next.add(key);
-            return next;
-        });
-    }, []);
+    const mappedHeadersByChecked = useMemo(() => {
+        return selectedHeaders.map(h => h.checked);
+    }, [selectedHeaders]);
 
-    const activeHeaders = useMemo(() => tableHeaders.filter(h => visibleHeaders.has(h)), [tableHeaders, visibleHeaders]);
+    const activeHeaders = useMemo(() => {
+        return selectedHeaders.filter(h => h.checked).map(h => h.name);
+    }, [selectedHeaders]);
 
     const { renderCell } = useColumns({
         dateKeys: DATE_KEYS,
@@ -59,39 +50,48 @@ export default function StableKnowledgeGraphDetailsOverview() {
     });
 
     return <>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-2 mb-4">
-            {tableHeaders.map(header => (
-                <label key={header} className="flex items-center gap-2 text-sm cursor-pointer">
-                    <input type="checkbox" onChange={() => toggleHeaders(header)} checked={visibleHeaders.has(header)} />
-                    {header}
-                </label>
-            ))}
-        </div>
-        <div className="inline-block min-w-full align-middle">
-            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg" style={{ padding: '3px' }}>
-                {stableData && tableHeaders.length > 0 && <table className="min-w-full divide-y divide-gray-300">
-                    <thead className="bg-gray-50">
-                        <tr>
-                            {activeHeaders.map(header => (
-                                <th key={header} className="border px-3 py-2 text-left text-sm font-medium">
-                                    {header}
-                                </th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody className="divide-y divide-gray-200">
-                        {stableData && stableData.map((row: any, rowIndex: number) => (
-                            <tr key={rowIndex} className={rowIndex % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
-                                {activeHeaders.map((header) => (
-                                    <td key={header} className="border px-3 py-2 text-sm text-gray-900">
-                                        {renderCell(header, row[header])}
-                                    </td>
-                                ))}
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>}
+        {stableData && <>
+            <p className="text-lg font-medium leading-6 text-gray-700 mb-4">Filter data</p>
+            <div className="flex flex-row items-end gap-x-4">
+                <div className="flex flex-col items-center">
+                    <label className="text-sm text-gray-700">Columns:</label>
+                    <KernDropdown options={selectedHeaders} buttonName={mappedHeadersByName.length > 0 ? mappedHeadersByName.join(', ') : 'Select columns'}
+                        hasCheckboxes={true} selectedCheckboxes={mappedHeadersByChecked} selectedOption={setSelectedHeaders} hasSelectAll={true} truncateButtonName={true}
+                        scrollAfterNOptions={10}
+                        dropdownWidth="w-[300px]"
+                        dropdownItemsClasses="w-[300px]" />
+                </div>
+
             </div>
-        </div>
+            <div className="inline-block min-w-full align-middle">
+                <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg" style={{ padding: '3px' }}>
+                    <table className="min-w-full divide-y divide-gray-300">
+                        <thead className="bg-gray-50">
+                            <tr>
+                                {activeHeaders.map(header => (
+                                    <th key={header} className="border px-3 py-2 text-left text-sm font-medium">
+                                        {header}
+                                    </th>
+                                ))}
+                                {activeHeaders.length === 0 && <th className="border px-3 py-2 text-left text-sm font-medium text-gray-500">
+                                    No columns selected.
+                                </th>}
+                            </tr>
+                        </thead>
+                        <tbody className="divide-y divide-gray-200">
+                            {stableData && stableData.map((row: any, rowIndex: number) => (
+                                <tr key={rowIndex} className={rowIndex % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
+                                    {activeHeaders.map((header) => (
+                                        <td key={header} className="border px-3 py-2 text-sm text-gray-900">
+                                            {renderCell(header, row[header])}
+                                        </td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>}
     </>
 }
